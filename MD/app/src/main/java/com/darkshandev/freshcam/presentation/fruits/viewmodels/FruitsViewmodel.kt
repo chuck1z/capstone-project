ace40356a2cc15b4df7d938afa0d5a5e398e8eec
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-18 09:31:13 +0700

    init fruits viewmodel

---
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/viewmodels/FruitsViewmodel.kt
@@ -0,0 +1,9 @@
+package com.darkshandev.freshcam.presentation.fruits.viewmodels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FruitsViewmodel @Inject constructor() :ViewModel(){
+}
