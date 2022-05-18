75031a9e5a7caf4c03d772d9e085314c40f4dc29
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-18 10:22:46 +0700

    refactor

--- MD/app/src/main/java/com/darkshandev/freshcam/data/datasources/ClassifierDatasource.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/datasources/ClassifierDatasource.kt
@@ -3,5 +3,4 @@ package com.darkshandev.freshcam.data.datasources
 import retrofit2.Retrofit
 import javax.inject.Inject
 
-class ClassifierDatasource @Inject constructor(private val retrofit: Retrofit) {
-}
+class ClassifierDatasource @Inject constructor(private val retrofit: Retrofit)
--- MD/app/src/main/java/com/darkshandev/freshcam/data/datasources/FruitsDatasource.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/datasources/FruitsDatasource.kt
@@ -3,5 +3,4 @@ package com.darkshandev.freshcam.data.datasources
 import retrofit2.Retrofit
 import javax.inject.Inject
 
-class FruitsDatasource @Inject constructor(private val retrofit: Retrofit) {
-}
+class FruitsDatasource @Inject constructor(private val retrofit: Retrofit)
