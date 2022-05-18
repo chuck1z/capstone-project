2d9a0a9cf658fd7127eb386070392c71878a609e
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-18 09:28:19 +0700

    init classifier viewmodel

---
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/viewmodels/ClassifierViewmodel.kt
@@ -0,0 +1,10 @@
+package com.darkshandev.freshcam.presentation.classifier.viewmodels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ClassifierViewmodel @Inject constructor() : ViewModel() {
+    
+}
