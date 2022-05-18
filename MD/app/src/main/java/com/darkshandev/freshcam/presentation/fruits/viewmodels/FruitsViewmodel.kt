30eccfb792b6e4f369faabbd53e78e8e9ef2f371
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-18 10:24:55 +0700

    inject repository into fruits viewmodel

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/viewmodels/FruitsViewmodel.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/viewmodels/FruitsViewmodel.kt
@@ -1,8 +1,9 @@
 package com.darkshandev.freshcam.presentation.fruits.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.darkshandev.freshcam.data.repositories.FruitsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class FruitsViewmodel @Inject constructor() : ViewModel()
+class FruitsViewmodel @Inject constructor(private val repo:FruitsRepository) : ViewModel()
