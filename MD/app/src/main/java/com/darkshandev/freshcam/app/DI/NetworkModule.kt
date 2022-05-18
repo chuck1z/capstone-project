93f0ad0fb06468d282c907b437bc840bc0619d2d
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-18 10:01:19 +0700

    setup network module

--- MD/app/src/main/java/com/darkshandev/freshcam/app/DI/NetworkModule.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/app/DI/NetworkModule.kt
@@ -1,4 +1,49 @@
 package com.darkshandev.freshcam.app.DI
 
+import com.darkshandev.freshcam.BuildConfig
+import com.darkshandev.freshcam.app.Config
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
 object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        } else {
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
+        return loggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkhttpClient(
+        interceptor: HttpLoggingInterceptor,
+
+    ): OkHttpClient = OkHttpClient
+        .Builder()
+        .addInterceptor(interceptor)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofitClient(
+        client: OkHttpClient
+    ): Retrofit = Retrofit
+        .Builder()
+        .baseUrl(Config.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
 }
