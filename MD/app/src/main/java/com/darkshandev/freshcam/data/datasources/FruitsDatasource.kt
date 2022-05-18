695877b655714cb03360b0834282daa907d518d6
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-18 10:17:28 +0700

    inject retrofit to fruits datasources

--- MD/app/src/main/java/com/darkshandev/freshcam/data/datasources/FruitsDatasource.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/datasources/FruitsDatasource.kt
@@ -1,4 +1,7 @@
 package com.darkshandev.freshcam.data.datasources
 
-class FruitsDatasource {
+import retrofit2.Retrofit
+import javax.inject.Inject
+
+class FruitsDatasource @Inject constructor(private val retrofit: Retrofit) {
 }
