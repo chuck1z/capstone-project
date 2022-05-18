36ae53d068da71deea9c6ba5c51e506ff99ac41e
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-18 10:24:50 +0700

    inject repository into classifier viewmodel

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/viewmodels/ClassifierViewmodel.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/viewmodels/ClassifierViewmodel.kt
@@ -1,8 +1,9 @@
 package com.darkshandev.freshcam.presentation.classifier.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.darkshandev.freshcam.data.repositories.ClassifierRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ClassifierViewmodel @Inject constructor() : ViewModel()
+class ClassifierViewmodel @Inject constructor(private val repository: ClassifierRepository) : ViewModel()
