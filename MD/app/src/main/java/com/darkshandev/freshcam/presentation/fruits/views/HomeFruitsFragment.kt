063782c4692a92d72482077d31d582340d75e895
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-05-22 21:48:14 +0700

    ui for home page

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/views/HomeFruitsFragment.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/views/HomeFruitsFragment.kt
@@ -4,11 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.darkshandev.freshcam.R
+import com.darkshandev.freshcam.databinding.FragmentHomeFruitsBinding
+import com.darkshandev.freshcam.presentation.fruits.viewmodels.FruitsViewmodel
 
 
 class HomeFruitsFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeFruitsBinding
+    private val fruitsViewmodel: FruitsViewmodel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentHomeFruitsBinding.inflate(layoutInflater)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
