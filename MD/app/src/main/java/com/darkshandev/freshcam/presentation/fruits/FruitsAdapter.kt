f7fa55c97c7cd39dabca54996f531cf7aa94d236
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-24 12:16:49 +0700

    implement diffutils

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/FruitsAdapter.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/FruitsAdapter.kt
@@ -4,24 +4,35 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.darkshandev.freshcam.data.FruitsTips
 import com.darkshandev.freshcam.databinding.ItemFruitsTipsBinding
+import com.darkshandev.freshcam.utils.FruitTipsDiffUtils
 
-class FruitsAdapter(private val listFruitsTips: List<FruitsTips>) : RecyclerView.Adapter<FruitsAdapter.ViewHolder>(){
+class FruitsAdapter() : RecyclerView.Adapter<FruitsAdapter.ViewHolder>(){
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
     private lateinit var binding: ItemFruitsTipsBinding
+    private var currentList= emptyList<FruitsTips>();
+    fun updateList(newList: List<FruitsTips>){
+       //update currentList with newList using FruitTipsDiffUtils
+        val diff = DiffUtil
+            .calculateDiff(FruitTipsDiffUtils(currentList, newList))
+        this.currentList = newList
 
+        diff.dispatchUpdatesTo(this)
+
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FruitsAdapter.ViewHolder {
         binding = ItemFruitsTipsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding.root)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val fruitsTips = listFruitsTips[position]
+        val fruitsTips = currentList[position]
         with(binding){
             Glide.with(binding.root.context)
                 .load(fruitsTips.photoUrl)
@@ -31,6 +42,6 @@ class FruitsAdapter(private val listFruitsTips: List<FruitsTips>) : RecyclerView
         }
     }
 
-    override fun getItemCount(): Int = listFruitsTips.size
+    override fun getItemCount(): Int = currentList.size
 
 }
