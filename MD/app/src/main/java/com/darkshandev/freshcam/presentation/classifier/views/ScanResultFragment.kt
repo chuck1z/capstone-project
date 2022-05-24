6fa486d0f699cd874aee07142a5c17d2de35cdd7
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-24 20:44:39 +0700

    implement collector flow

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/views/ScanResultFragment.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/views/ScanResultFragment.kt
@@ -1,21 +1,67 @@
 package com.darkshandev.freshcam.presentation.classifier.views
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.darkshandev.freshcam.R
+import androidx.core.net.toUri
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.darkshandev.freshcam.data.models.AppState
+import com.darkshandev.freshcam.data.models.getFreshness
+import com.darkshandev.freshcam.data.models.getName
+import com.darkshandev.freshcam.databinding.FragmentScanResultBinding
+import com.darkshandev.freshcam.presentation.classifier.viewmodels.ClassifierViewmodel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.coroutines.launch
 
 
-class ScanResultFragment : Fragment() {
+class ScanResultFragment : BottomSheetDialogFragment() {
+    private val classifierViewmodel: ClassifierViewmodel by activityViewModels()
+    private var binding: FragmentScanResultBinding? = null
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentScanResultBinding.inflate(layoutInflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan_result, container, false)
+        lifecycleScope.launch {
+            classifierViewmodel.image.flowWithLifecycle(lifecycle).collect {
+                it?.let {
+                    binding?.imageView3?.setImageURI(it.toUri())
+                }
+            }
+        }
+        lifecycleScope.launch {
+            classifierViewmodel.result.flowWithLifecycle(lifecycle).collect { state ->
+                when (state) {
+                    is AppState.Success -> {
+                        binding?.apply {
+                            state.data?.let {
+                                label.text = it.getFreshness()
+                                confidence.text = "${it.confidence}%"
+                                descInfo.text = it.description
+                                name.text = it.getName()
+
+                            }
+                        }
+                    }
+                    else -> {}
+                }
+
+            }
+        }
+        return binding?.root
     }
 
 
