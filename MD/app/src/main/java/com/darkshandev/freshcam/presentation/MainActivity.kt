0d2b1cf0e223f0f1665dd28e519ff080442f3df9
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-24 10:31:20 +0700

    add navCOntroller destination changed listener

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/MainActivity.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.darkshandev.freshcam.presentation
 
+import android.Manifest
 import android.animation.ObjectAnimator
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnticipateInterpolator
@@ -8,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.activity.viewModels
 import androidx.core.animation.doOnEnd
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
@@ -28,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navHostFragment.navController
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
@@ -42,12 +47,55 @@ class MainActivity : AppCompatActivity() {
         }
         setContentView(binding.root)
         setupNavigation()
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
     }
 
     private fun setupNavigation() {
         //setup NavigationUi with bottomNavigationView
         NavigationUI.setupWithNavController(binding.bottomNavigationView, navController,)
         binding.bottomNavigationView.itemIconTintList = null
+        binding.fab.setOnClickListener {
+            navController.navigate(R.id.action_homeFruitsFragment_to_scanFruitsFragment)
+        }
+navController.addOnDestinationChangedListener { _, destination, _ ->
+    when (destination.id) {
+        R.id.homeFruitsFragment -> {
+            binding.fab.visibility = View.VISIBLE
+            binding.bottomNavigationView.visibility = View.VISIBLE
+            binding.fab.setOnClickListener {
+                navController.navigate(R.id.action_homeFruitsFragment_to_scanFruitsFragment)
+            }
+        }
+        R.id.settingFragment -> {
+            binding.fab.visibility = View.VISIBLE
+            binding.bottomNavigationView.visibility = View.VISIBLE
+            binding.fab.setOnClickListener {
+                navController.navigate(R.id.action_settingFragment_to_scanFruitsFragment)
+            }
+        }
+        R.id.scanFruitsFragment -> {
+            binding.fab.visibility = View.GONE
+            binding.bottomNavigationView.visibility = View.GONE
+        }
+    }
+
+    }
+    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            Manifest.permission.CAMERA,
+        )
+        private const val REQUEST_CODE_PERMISSIONS = 10
 
     }
 }
