03bda7025a576d82726e35240eb3bf8e47cdd61d
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-24 20:41:30 +0700

    add scan result model

---
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/models/ScanResult.kt
@@ -0,0 +1,13 @@
+package com.darkshandev.freshcam.data.models
+
+data class ScanResult(
+    val label:String,
+    val confidence:Float,
+    val description:String
+)
+fun ScanResult.getName():String {
+    return label.split("_").last()
+}
+fun ScanResult.getFreshness():String {
+    return label.split("_").first()
+}
