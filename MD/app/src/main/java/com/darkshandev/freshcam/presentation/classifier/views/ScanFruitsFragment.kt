fbd7038dd11f13808b8ee0a20cf7855e0e2a5c7b
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-24 20:42:57 +0700

    implement classify method

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/views/ScanFruitsFragment.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/views/ScanFruitsFragment.kt
@@ -3,20 +3,27 @@ package com.darkshandev.freshcam.presentation.classifier.views
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
-import androidx.camera.core.TorchState.OFF
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
 import androidx.camera.core.TorchState.ON
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.darkshandev.freshcam.R
 import com.darkshandev.freshcam.databinding.FragmentScanFruitsBinding
+import com.darkshandev.freshcam.presentation.classifier.viewmodels.ClassifierViewmodel
 import com.darkshandev.freshcam.utils.createFile
 import com.darkshandev.freshcam.utils.uriToFile
 import java.io.File
@@ -24,12 +31,13 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class ScanFruitsFragment : Fragment() {
-private var binding: FragmentScanFruitsBinding? = null
+    private var binding: FragmentScanFruitsBinding? = null
     private var cameraExecutor: ExecutorService? = null
     private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
     private var imageCapture: ImageCapture? = null
+    private val classifierViewmodel: ClassifierViewmodel by activityViewModels()
     override fun onDestroyView() {
-        binding=null
+        binding = null
         cameraExecutor?.shutdown()
         cameraExecutor = null
         super.onDestroyView()
@@ -39,18 +47,20 @@ private var binding: FragmentScanFruitsBinding? = null
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-  binding = FragmentScanFruitsBinding.inflate(inflater, container, false)
+        binding = FragmentScanFruitsBinding.inflate(inflater, container, false)
 
         prepareCamera()
         setupView()
         return binding?.root
     }
+
     override fun onResume() {
         super.onResume()
 
-            startCamera()
+        startCamera()
 
     }
+
     private fun prepareCamera() {
         cameraExecutor = Executors.newSingleThreadExecutor()
 //        imageCapture = ImageCapture.Builder()
@@ -59,6 +69,7 @@ private var binding: FragmentScanFruitsBinding? = null
 //            .build()
 
     }
+
     private fun startCamera() {
 
         binding?.apply {
@@ -93,22 +104,22 @@ private var binding: FragmentScanFruitsBinding? = null
 
     private fun setupView() {
         binding?.apply {
-         backButton.setOnClickListener {
-            activity?.onBackPressed()
-         }
+            backButton.setOnClickListener {
+                activity?.onBackPressed()
+            }
             captureImage.setOnClickListener {
                 captureImage()
             }
             flashButton.setOnClickListener {
-            imageCapture?.apply {
-                if(flashMode== ON){
-                    flashMode= ImageCapture.FLASH_MODE_OFF
-                    flashButton.setImageResource(R.drawable.ic_baseline_flash_off_24)
-                }else{
-                    flashMode= ImageCapture.FLASH_MODE_ON
-                    flashButton.setImageResource(R.drawable.ic_baseline_flash_on_24)
+                imageCapture?.apply {
+                    if (flashMode == ON) {
+                        flashMode = ImageCapture.FLASH_MODE_OFF
+                        flashButton.setImageResource(R.drawable.ic_baseline_flash_off_24)
+                    } else {
+                        flashMode = ImageCapture.FLASH_MODE_ON
+                        flashButton.setImageResource(R.drawable.ic_baseline_flash_on_24)
+                    }
                 }
-            }
             }
             galleryButton.setOnClickListener {
                 val intent = Intent()
@@ -119,6 +130,7 @@ private var binding: FragmentScanFruitsBinding? = null
             }
         }
     }
+
     private fun captureImage() {
         val imageCapture = imageCapture ?: return
         val photoFile = createFile(requireActivity().application)
@@ -142,6 +154,7 @@ private var binding: FragmentScanFruitsBinding? = null
             }
         )
     }
+
     private val launcherIntentGallery = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -151,7 +164,15 @@ private var binding: FragmentScanFruitsBinding? = null
             classifyFruitsByThis(image = myFile)
         }
     }
-    private fun classifyFruitsByThis(image: File){
+
+    private fun classifyFruitsByThis(image: File) {
+        try {
+            classifierViewmodel.setImage(image)
+            classifierViewmodel.classifyImage()
+            findNavController().navigate(R.id.action_scanFruitsFragment_to_scanResultFragment)
+        }catch (e:java.lang.Exception){
+            e.message?.let { Log.e("Error", it )}
+        }
 
 
     }
