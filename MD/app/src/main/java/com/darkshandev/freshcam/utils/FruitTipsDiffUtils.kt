43d616efb39fab85a3d55d136d042e2504bf9aef
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-24 12:16:34 +0700

    create diffcallback for FruitsTips

---
+++ MD/app/src/main/java/com/darkshandev/freshcam/utils/FruitTipsDiffUtils.kt
@@ -0,0 +1,23 @@
+package com.darkshandev.freshcam.utils
+
+import androidx.recyclerview.widget.DiffUtil
+import com.darkshandev.freshcam.data.FruitsTips
+
+class FruitTipsDiffUtils(private val oldList: List<FruitsTips>, private val newList: List<FruitsTips>) :
+    DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList == newList
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val old = oldList[oldItemPosition]
+        val latest = newList[newItemPosition]
+        return when (old.title) {
+            latest.title -> true
+            else -> false
+        }
+    }
+}
