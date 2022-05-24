a6189dc6cd7c369702e49c371463b4387e30d668
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-24 10:35:07 +0700

    preparing camera

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/views/ScanFruitsFragment.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/views/ScanFruitsFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
 import com.darkshandev.freshcam.R
 import com.darkshandev.freshcam.databinding.FragmentScanFruitsBinding
+import java.util.concurrent.ExecutorService
 
 class ScanFruitsFragment : Fragment() {
 private var binding: FragmentScanFruitsBinding? = null
+    private var cameraExecutor: ExecutorService? = null
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    private var imageCapture: ImageCapture? = null
     override fun onDestroyView() {
         binding=null
+        cameraExecutor?.shutdown()
+        cameraExecutor = null
         super.onDestroyView()
     }
 
