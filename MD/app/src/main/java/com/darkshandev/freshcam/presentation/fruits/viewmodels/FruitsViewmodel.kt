1fe6d84128f748aeb12ef95f2b53a09879316994
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-24 20:43:32 +0700

    refactor

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/viewmodels/FruitsViewmodel.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/viewmodels/FruitsViewmodel.kt
@@ -3,7 +3,7 @@ package com.darkshandev.freshcam.presentation.fruits.viewmodels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.darkshandev.freshcam.data.FruitsTips
+import com.darkshandev.freshcam.data.models.FruitsTips
 import com.darkshandev.freshcam.data.repositories.FruitsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
