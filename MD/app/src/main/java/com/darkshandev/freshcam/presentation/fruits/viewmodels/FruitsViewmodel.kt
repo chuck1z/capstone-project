1e0cd4a0ada02186c78f13e4924f4e24905b9f14
IzumiShaka-desu <shakaaji29@gmail.com>
2022-05-28 12:58:35 +0700

    add app state on viewmodel

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/viewmodels/FruitsViewmodel.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/viewmodels/FruitsViewmodel.kt
@@ -1,15 +1,39 @@
 package com.darkshandev.freshcam.presentation.fruits.viewmodels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.darkshandev.freshcam.data.models.AppState
+import com.darkshandev.freshcam.data.models.FruitsDetail
 import com.darkshandev.freshcam.data.models.FruitsTips
 import com.darkshandev.freshcam.data.repositories.FruitsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
 class FruitsViewmodel @Inject constructor(private val repo: FruitsRepository) : ViewModel() {
-    private val _fruits = MutableLiveData<List<FruitsTips>>()
-    val fruits: LiveData<List<FruitsTips>> = _fruits
+//    private val _fruits = MutableLiveData<List<FruitsTips>>()
+//    val fruits: LiveData<List<FruitsTips>> = _fruits
+//    private val _isLoading = MutableLiveData<Boolean>()
+    private val _fruits = MutableStateFlow<AppState<List<FruitsTips>>>(AppState.Initial())
+     val fruits = _fruits.asStateFlow()
+    private val _selectedFruitsId = MutableStateFlow<String>("")
+    fun setSelectedFruitsId(id: String) {
+        _selectedFruitsId.value = id
+    }
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val fruitsDetail:StateFlow<AppState<FruitsDetail>> =_selectedFruitsId
+        .distinctUntilChanged { old, new -> old == new && new == "" }
+        .transformLatest { id ->
+            repo.getFruitsDetail(id)
+                .collect { result ->
+                    emit(result)
+                }
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.Lazily,
+            AppState.Loading()
+        )
+
 }
