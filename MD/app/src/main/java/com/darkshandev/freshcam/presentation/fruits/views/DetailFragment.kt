f44a69aa7356521370cc22b91b0e885e9a9be170
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-02 16:14:13 +0700

    refactor

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/views/DetailFragment.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/views/DetailFragment.kt
@@ -8,47 +8,46 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import com.darkshandev.freshcam.R
 import com.darkshandev.freshcam.data.models.AppState
 import com.darkshandev.freshcam.databinding.FragmentDetailBinding
 import com.darkshandev.freshcam.presentation.fruits.viewmodels.FruitsViewmodel
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class DetailFragment : Fragment() {
     private val fruitsViewmodel by activityViewModels<FruitsViewmodel>()
-private var binding: FragmentDetailBinding? = null
+    private var binding: FragmentDetailBinding? = null
     override fun onDestroy() {
-        binding=null
+        binding = null
         super.onDestroy()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=FragmentDetailBinding.inflate(layoutInflater)
+        binding = FragmentDetailBinding.inflate(layoutInflater)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         lifecycleScope.launch {
-            fruitsViewmodel.fruitsDetail.flowWithLifecycle(lifecycle).collect{state->
-                when(state){
-                    is AppState.Loading->{
-                       //activate loading view
-                            
+            fruitsViewmodel.fruitsDetail.flowWithLifecycle(lifecycle).collect { state ->
+                when (state) {
+                    is AppState.Loading -> {
+                        //activate loading view
+
                     }
-                    is AppState.Success->{
+                    is AppState.Success -> {
                         binding?.apply {
-                           state.data?.let {
-                               tvFruitTitle.text=it.name
-                               tvAboutDesc.text=it.about
-                               tvScientificName.text=it.binom
-                           }
+                            state.data?.let {
+                                tvFruitTitle.text = it.name
+                                tvAboutDesc.text = it.about
+                                tvScientificName.text = it.binom
+                            }
                         }
                     }
-                    is AppState.Error->{
+                    is AppState.Error -> {
                         //activate error view
                     }
                     is AppState.Initial -> {
@@ -57,7 +56,7 @@ private var binding: FragmentDetailBinding? = null
                 }
             }
         }
-return binding?.root
+        return binding?.root
     }
 
 
