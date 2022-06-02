c73ad86239c40b257b003829a18ac07e11149712
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-02 16:25:59 +0700

    add get response method

--- MD/app/src/main/java/com/darkshandev/freshcam/data/datasources/ClassifierDatasource.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/datasources/ClassifierDatasource.kt
@@ -1,6 +1,47 @@
 package com.darkshandev.freshcam.data.datasources
 
+import android.content.Context
+import com.darkshandev.freshcam.R
+import com.darkshandev.freshcam.data.models.AppState
+import com.darkshandev.freshcam.utils.ErrorUtils
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Response
 import retrofit2.Retrofit
 import javax.inject.Inject
 
-class ClassifierDatasource @Inject constructor(private val retrofit: Retrofit)
+
+class ClassifierDatasource @Inject constructor(
+    private val retrofit: Retrofit,
+    @ApplicationContext private val context: Context
+) {
+
+
+
+    private suspend fun <T> getResponse(
+        defaultErrorMessage: String,
+        request: suspend () -> Response<T>
+
+    ): AppState<T> {
+        return try {
+            val result = request.invoke()
+            if (result.isSuccessful) {
+                val body = result.body()
+                body?.let {
+                    return AppState.Success(it)
+                } ?: run {
+                    return error("${result.code()} ${result.message()}")
+                }
+            } else {
+
+                val errorResponse = ErrorUtils.parseError(result, retrofit)
+                error(errorResponse?.localizedMessage ?: defaultErrorMessage)
+
+            }
+        } catch (e: Throwable) {
+            AppState.Error(context.getString(R.string.error_request), null)
+        }
+    }
+
+    private fun <T> error(errorMessage: String): AppState<T> =
+        AppState.Error(errorMessage)
+}
