659c1703ee48b6bded2f55b97cd50222287b8cbe
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-02 16:30:58 +0700

    implement get latest model on repository

--- MD/app/src/main/java/com/darkshandev/freshcam/data/repositories/ClassifierRepository.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/repositories/ClassifierRepository.kt
@@ -1,7 +1,10 @@
 package com.darkshandev.freshcam.data.repositories
 
 import com.darkshandev.freshcam.data.datasources.ClassifierDatasource
+import com.darkshandev.freshcam.data.models.AppState
 import com.darkshandev.freshcam.data.models.ScanResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
 import javax.inject.Inject
 
@@ -9,6 +12,10 @@ class ClassifierRepository @Inject constructor(private val dataSource: Classifie
     interface ClassifierCallback {
         fun onSuccess(result: ScanResult)
         fun onError(error: String)
+    }
+
+    suspend fun getLatestModel() {
+        dataSource.getLatestModel()
     }
 
     fun classifyImage(value: File, callback: ClassifierCallback) {
