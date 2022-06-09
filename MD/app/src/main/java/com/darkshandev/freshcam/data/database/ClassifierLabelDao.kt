d24d414b0ff6412bfa67a028283de3759ca23a50
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 09:19:53 +0700

    add classifier label dao

--- MD/app/src/main/java/com/darkshandev/freshcam/data/database/ClassifierLabelDao.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/database/ClassifierLabelDao.kt
@@ -1,4 +1,15 @@
 package com.darkshandev.freshcam.data.database
 
-interface LabelDao {
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.darkshandev.freshcam.data.models.ClassifierLabelEntity
+import kotlinx.coroutines.flow.Flow
+
+
+interface ClassifierLabelDao {
+    @Query("SELECT * FROM ClassifierLabelEntity")
+   suspend fun getLabels(): Flow<List<ClassifierLabelEntity>>
+   @Insert(onConflict = OnConflictStrategy.REPLACE)
+   suspend fun update(rows: List<ClassifierLabelEntity>)
 }
