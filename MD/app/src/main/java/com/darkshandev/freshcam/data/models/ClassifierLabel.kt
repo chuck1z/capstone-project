0260bbb75748b7e37bc2e23d8bf68519d8869816
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 09:23:18 +0700

    add classifier label

--- MD/app/src/main/java/com/darkshandev/freshcam/data/models/ClassifierLabel.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/models/ClassifierLabel.kt
@@ -1,3 +1,20 @@
 package com.darkshandev.freshcam.data.models
 
-data class ClassifierLabel()
+import com.google.gson.annotations.SerializedName
+
+data class ClassifierLabel(
+val id: Int,
+val index: Int,
+val label: String,
+@SerializedName("short_desc")
+val shortDesc: String
+)
+
+fun ClassifierLabel.toEntity(): ClassifierLabelEntity {
+    return ClassifierLabelEntity(
+        id = id,
+        index = index,
+        label = label,
+        shortDesc = shortDesc
+    )
+}
