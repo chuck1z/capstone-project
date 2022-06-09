7df009bba53c70cb1fc997b903a416ecc5870a34
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-10 06:03:36 +0700

    add all required method on fruits repository

--- MD/app/src/main/java/com/darkshandev/freshcam/data/repositories/FruitsRepository.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/repositories/FruitsRepository.kt
@@ -1,8 +1,7 @@
 package com.darkshandev.freshcam.data.repositories
 
 import com.darkshandev.freshcam.data.datasources.FruitsDatasource
-import com.darkshandev.freshcam.data.models.AppState
-import com.darkshandev.freshcam.data.models.FruitsDetail
+import com.darkshandev.freshcam.data.models.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -14,25 +13,117 @@ class FruitsRepository @Inject constructor(private val dataSource: FruitsDatasou
         flow<AppState<FruitsDetail>> {
             emit(AppState.Loading())
             val result = dataSource.getFruitsDetail(fruitsId)
-            when (result) {
-                is AppState.Success -> {
-                    result.data?.let {
-                        if (it.error) {
-                            emit(AppState.Error(it.message))
-                        } else {
-                            emit(AppState.Success(it.data))
+            try {
+
+                when (result) {
+                    is AppState.Success -> {
+                        result.data?.let {
+                            if (it.error) {
+                                emit(AppState.Error(it.message))
+                            } else {
+                                emit(AppState.Success(it.data))
+                            }
+                        } ?: run {
+                            emit(AppState.Error(message = ("No data found")))
                         }
-                    } ?: run {
-                        emit(AppState.Error(message = ("No data found")))
+                    }
+                    is AppState.Error -> emit(result.message?.let {
+                        AppState.Error(it)
+                    } ?: AppState.Error(message = "Unknown Error")
+                    )
+                    else -> {
+                        emit(AppState.Error(message = "Unknown error"))
                     }
                 }
-                is AppState.Error -> emit(result.message?.let {
-                    AppState.Error(it)
-                } ?: AppState.Error(message = "Unknown Error")
-                )
-                else -> {
-                    emit(AppState.Error(message = "Unknown error"))
+            } catch (e: Exception) {
+                emit(AppState.Error(message = "Unknown error"))
+            }
+        }.flowOn(Dispatchers.IO)
+    fun getTipsDetail(tipsId: String): Flow<AppState<TipsDetail>> =
+        flow<AppState<TipsDetail>> {
+            emit(AppState.Loading())
+            val result = dataSource.getTipsDetail(tipsId)
+            try {
+                when (result) {
+                    is AppState.Success -> {
+                        result.data?.let {
+                            if (it.error) {
+                                emit(AppState.Error(it.message))
+                            } else {
+                                emit(AppState.Success(it.data))
+                            }
+                        } ?: run {
+                            emit(AppState.Error(message = ("No data found")))
+                        }
+                    }
+                    is AppState.Error -> emit(result.message?.let {
+                        AppState.Error(it)
+                    } ?: AppState.Error(message = "Unknown Error")
+                    )
+                    else -> {
+                        emit(AppState.Error(message = "Unknown error"))
+                    }
                 }
+            } catch (e: Exception) {
+                emit(AppState.Error(message = "Unknown error"))
+            }
+        }.flowOn(Dispatchers.IO)
+    fun getTips(): Flow<AppState<List<Tips>>> =
+        flow<AppState<List<Tips>>> {
+            emit(AppState.Loading())
+            val result = dataSource.getTips()
+            try {
+                when (result) {
+                    is AppState.Success -> {
+                        result.data?.let {
+                            if (it.error) {
+                                emit(AppState.Error(it.message))
+                            } else {
+                                emit(AppState.Success(it.data))
+                            }
+                        } ?: run {
+                            emit(AppState.Error(message = ("No data found")))
+                        }
+                    }
+                    is AppState.Error -> emit(result.message?.let {
+                        AppState.Error(it)
+                    } ?: AppState.Error(message = "Unknown Error")
+                    )
+                    else -> {
+                        emit(AppState.Error(message = "Unknown error"))
+                    }
+                }
+            } catch (e: Exception) {
+                emit(AppState.Error(message = "Unknown error"))
+            }
+        }.flowOn(Dispatchers.IO)
+    fun getFruitsOfTheDay(): Flow<AppState<FruitOfTheDay> =
+        flow<AppState<FruitOfTheDay>> {
+            emit(AppState.Loading())
+            val result = dataSource.getFruitsOfTheDay()
+            try {
+                when (result) {
+                    is AppState.Success -> {
+                        result.data?.let {
+                            if (it.error) {
+                                emit(AppState.Error(it.message))
+                            } else {
+                                emit(AppState.Success(it.data))
+                            }
+                        } ?: run {
+                            emit(AppState.Error(message = ("No data found")))
+                        }
+                    }
+                    is AppState.Error -> emit(result.message?.let {
+                        AppState.Error(it)
+                    } ?: AppState.Error(message = "Unknown Error")
+                    )
+                    else -> {
+                        emit(AppState.Error(message = "Unknown error"))
+                    }
+                }
+            } catch (e: Exception) {
+                emit(AppState.Error(message = "Unknown error"))
             }
         }.flowOn(Dispatchers.IO)
 }
