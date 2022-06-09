d76b970e19350d4988fae592fa5d884c92f25937
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 17:31:24 +0700

    add diffutils for history

---
+++ MD/app/src/main/java/com/darkshandev/freshcam/utils/HistoryClassificationDiffUtils.kt
@@ -0,0 +1,27 @@
+package com.darkshandev.freshcam.utils
+
+import androidx.recyclerview.widget.DiffUtil
+import com.darkshandev.freshcam.data.models.FruitsTips
+import com.darkshandev.freshcam.data.models.HistoryClassificationEntity
+
+class HistoryClassificationDiffUtils(
+    private val oldList: List<HistoryClassificationEntity>,
+    private val newList: List<HistoryClassificationEntity>
+) :
+    DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldList.size
+
+    override fun getNewListSize(): Int = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldList == newList
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val old = oldList[oldItemPosition]
+        val latest = newList[newItemPosition]
+        return when (old.id) {
+            latest.id -> true
+            else -> false
+        }
+    }
+}
