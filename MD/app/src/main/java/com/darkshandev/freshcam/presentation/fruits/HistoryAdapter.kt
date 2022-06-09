fc4d663ce49d3d8683f00adea4b49816d9cc30bc
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 17:30:32 +0700

    create concrete implementation on adapter

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/HistoryAdapter.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/fruits/HistoryAdapter.kt
@@ -3,14 +3,29 @@ package com.darkshandev.freshcam.presentation.fruits
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.darkshandev.freshcam.data.models.HistoryClassificationEntity
 import com.darkshandev.freshcam.databinding.ItemHistoryBinding
+import com.darkshandev.freshcam.utils.HistoryClassificationDiffUtils
+import com.darkshandev.freshcam.utils.asPercentage
+import java.io.File
 
 class HistoryAdapter : RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
     private lateinit var binding: ItemHistoryBinding
+
+    private var currentList = emptyList<HistoryClassificationEntity>()
+    fun updateList(newList: List<HistoryClassificationEntity>) {
+        //update currentList with newList using HistoryClassificationDiffUtils
+        val diff = DiffUtil
+            .calculateDiff(HistoryClassificationDiffUtils(currentList, newList))
+        this.currentList = newList
+        diff.dispatchUpdatesTo(this)
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryAdapter.ViewHolder {
         binding = ItemHistoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -18,16 +33,20 @@ class HistoryAdapter : RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: HistoryAdapter.ViewHolder, position: Int) {
+        val history = currentList[position]
         with(binding) {
             Glide.with(binding.root.context)
-//                .load()
-//                .into(ivFruitsHistory)
-//            tvFruitName.text =
-//            tvDetection.text =
+                .load(File(history.photo))
+                .into(ivFruitsHistory)
+            val text =
+                "${history.confidence.asPercentage()} ${if (history.freshness) "Fresh" else "Rotten"}"
+            tvFruitName.text = history.fruitsName
+            tvDetection.text = text
         }
+
+
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = currentList.size
+
 }
