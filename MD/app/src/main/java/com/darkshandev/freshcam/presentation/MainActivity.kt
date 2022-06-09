c07f2301593b8c125f852910fc458ef46a9c8595
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 23:03:27 +0700

    add user guide

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/MainActivity.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.core.animation.doOnEnd
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.darkshandev.freshcam.R
@@ -44,6 +47,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
         setContentView(binding.root)
+        lifecycleScope.launchWhenCreated {
+            classifierViewmodel.isFirstLaunch.collect{
+                if(it){
+                    navController.navigate(R.id.action_homeFruitsFragment_to_oneTimeSetUpFragment)
+                }
+            }
+        }
         classifierViewmodel.getLatestLabel()
         setupNavigation()
         if (!allPermissionsGranted()) {
@@ -79,7 +89,7 @@ class MainActivity : AppCompatActivity() {
                         navController.navigate(R.id.action_settingFragment_to_scanFruitsFragment)
                     }
                 }
-                R.id.scanFruitsFragment,R.id.historyFragment,R.id.aboutFragment -> {
+                R.id.scanFruitsFragment,R.id.userGuideFragment,R.id.oneTimeSetUpFragment,R.id.historyFragment,R.id.aboutFragment -> {
                    hideBottomNav()
                 }
             }
