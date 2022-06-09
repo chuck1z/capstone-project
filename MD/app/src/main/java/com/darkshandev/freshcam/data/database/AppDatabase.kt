f5b13c948cbd260959f4cb67e93f45d8dfabba69
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 15:06:12 +0700

    add method on app datase to get history dao

--- MD/app/src/main/java/com/darkshandev/freshcam/data/database/AppDatabase.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/database/AppDatabase.kt
@@ -3,13 +3,15 @@ package com.darkshandev.freshcam.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.darkshandev.freshcam.data.models.ClassifierLabelEntity
+import com.darkshandev.freshcam.data.models.HistoryClassificationEntity
 
 
 @Database(
-    entities = [ClassifierLabelEntity::class],
+    entities = [ClassifierLabelEntity::class,HistoryClassificationEntity::class],
     version = 1,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun classifierLabelDao(): ClassifierLabelDao
+abstract fun  historyClassificationDao():HistoryClassificationDao
 }
