7646d43cf3d229c2a18aef9fd54fb7efe522551a
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-10 05:35:02 +0700

    add get fruit detail

--- MD/app/src/main/java/com/darkshandev/freshcam/data/networks/FruitsService.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/networks/FruitsService.kt
@@ -1,14 +1,16 @@
 package com.darkshandev.freshcam.data.networks
 
+import com.darkshandev.freshcam.data.models.FruitDetailResponse
 import com.darkshandev.freshcam.data.models.FruitsModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
-interface StoryService {
-    @GET("fruits")
-    suspend fun getFruits(
-        @Query("page") page: Int? = null,
-        @Query("size") size: Int? = null,
-    ): Response<List<FruitsModel>>
+interface FruitsService {
+    @GET("fruits/{id}/detail")
+    suspend fun getFruitDetail(
+        @Path("id") id:String
+    ): Response<FruitDetailResponse>
+   
 }
