d158c7300a6cf2e85b2de499e0b100dde7ab3f67
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 17:32:53 +0700

    add onclick for navigation

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/settings/views/SettingFragment.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/settings/views/SettingFragment.kt
@@ -5,17 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.darkshandev.freshcam.R
+import com.darkshandev.freshcam.databinding.FragmentSettingBinding
 
 
 class SettingFragment : Fragment() {
+    private var _binding:FragmentSettingBinding? = null
+    override fun onDestroy() {
+        _binding=null
+        super.onDestroy()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        _binding=FragmentSettingBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+      _binding?.apply {
+          cvHistory.setOnClickListener {
+              findNavController().navigate(R.id.action_settingFragment_to_historyFragment)
+          }
+          cvAbout.setOnClickListener {
+              findNavController().navigate(R.id.action_settingFragment_to_aboutFragment)
+          }
+          cvGuide.setOnClickListener {
+
+          }
+      }
+        return _binding?.root
     }
 
 }
