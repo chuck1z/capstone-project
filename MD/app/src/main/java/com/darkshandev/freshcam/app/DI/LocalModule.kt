5a141efef7163fc0b1a92a2bd43d5c51b8f23028
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 09:20:08 +0700

    add dao

--- MD/app/src/main/java/com/darkshandev/freshcam/app/DI/LocalModule.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/app/DI/LocalModule.kt
@@ -1,17 +1,17 @@
-package com.darkshandev.sutori.app.di
+package com.darkshandev.freshcam.app.DI
 
 import android.content.Context
 import androidx.room.Room
-import com.darkshandev.sutori.app.Config
-import com.darkshandev.sutori.data.database.AppDatabase
-import com.darkshandev.sutori.data.database.RemoteKeysDao
-import com.darkshandev.sutori.data.database.StoryDao
+import com.darkshandev.freshcam.app.Config
+import com.darkshandev.freshcam.data.database.AppDatabase
+import com.darkshandev.freshcam.data.database.ClassifierLabelDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -26,14 +26,7 @@ object LocalModule {
             AppDatabase::class.java,
             Config.DB_Name
         ).build()
-
     @Provides
-    fun provideStoryDao(appDatabase: AppDatabase): StoryDao {
-        return appDatabase.storyDao()
-    }
-
-    @Provides
-    fun provideRemoteKeysDao(appDatabase: AppDatabase): RemoteKeysDao {
-        return appDatabase.getRepoDao()
-    }
+    @Singleton
+    fun provideClassifierLabelDao(appDatabase: AppDatabase): ClassifierLabelDao = appDatabase.classifierLabelDao()
 }
