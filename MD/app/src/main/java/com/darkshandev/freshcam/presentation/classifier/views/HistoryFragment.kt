e87b2d5b79e162e9f4d91524a62d3097b449e159
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 17:31:02 +0700

    implement history list to recycler view

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/views/HistoryFragment.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/classifier/views/HistoryFragment.kt
@@ -5,15 +5,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.darkshandev.freshcam.R
+import com.darkshandev.freshcam.databinding.FragmentHistoryBinding
+import com.darkshandev.freshcam.presentation.classifier.viewmodels.ClassifierViewmodel
+import com.darkshandev.freshcam.presentation.fruits.HistoryAdapter
 
 class HistoryFragment : Fragment() {
+    private val classifierViewmodel:ClassifierViewmodel by activityViewModels()
+    private var _binding:FragmentHistoryBinding? = null
+    private val historyAdapter: HistoryAdapter = HistoryAdapter()
+
+    override fun onDestroy() {
+        _binding=null
+        super.onDestroy()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        _binding=FragmentHistoryBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        setUpView()
+        lifecycleScope.launchWhenResumed {
+            classifierViewmodel.classificationHistory.collect{
+                historyAdapter.updateList(it)
+            }
+        }
+        return _binding?.root
+    }
+
+    private fun setUpView() {
+        _binding?.apply {
+            rvHistory.apply {
+                layoutManager=LinearLayoutManager(requireActivity())
+                adapter=historyAdapter
+            }
+        }
     }
 
 }
