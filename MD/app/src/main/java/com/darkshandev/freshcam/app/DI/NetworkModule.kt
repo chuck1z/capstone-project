2aaf03e1ab362d28f5926d86f57633bce74c9c88
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 09:20:46 +0700

    add firebase config to network module

--- MD/app/src/main/java/com/darkshandev/freshcam/app/DI/NetworkModule.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/app/DI/NetworkModule.kt
@@ -2,7 +2,11 @@ package com.darkshandev.freshcam.app.DI
 
 import com.darkshandev.freshcam.BuildConfig
 import com.darkshandev.freshcam.app.Config
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.ml.modeldownloader.FirebaseModelDownloader
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -40,7 +44,17 @@ object NetworkModule {
     @Singleton
     @Provides
     fun provideModelDownloader(): FirebaseModelDownloader = FirebaseModelDownloader.getInstance()
-
+    @Singleton
+    @Provides
+    fun provideRemoteConfig():FirebaseRemoteConfig {
+        val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.fetchAndActivate()
+        return remoteConfig
+    }
     @Singleton
     @Provides
     fun provideRetrofitClient(
