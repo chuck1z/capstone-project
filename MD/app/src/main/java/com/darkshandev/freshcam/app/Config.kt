87829dd07070becffc0861e8ccd6898906ec5b71
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-09 14:04:21 +0700

    set base url configuration

--- MD/app/src/main/java/com/darkshandev/freshcam/app/Config.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/app/Config.kt
@@ -2,5 +2,5 @@ package com.darkshandev.freshcam.app
 
 object Config {
     val DB_Name="freshCam.db"
-    const val BASE_URL = "https://api.unsplash.com/"
+    const val BASE_URL = "https://backend-wgxdvk5ssq-de.a.run.app/"
 }
