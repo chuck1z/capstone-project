535fcca997e46bf6ac4127baf984f5b0d964ea9c
Sitifatimah121 <sitifatimah.ft.13@gmail.com>
2022-06-10 18:21:52 +0700

    user guide added

--- MD/app/src/main/java/com/darkshandev/freshcam/presentation/settings/views/UserGuideFragment.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/presentation/settings/views/UserGuideFragment.kt
@@ -5,56 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.darkshandev.freshcam.R
+import com.darkshandev.freshcam.databinding.FragmentUserGuideBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UserGuideFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UserGuideFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var binding: FragmentUserGuideBinding? = null
+
+    override fun onDestroy() {
+        binding = null
+        super.onDestroy()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        binding = FragmentUserGuideBinding.inflate(layoutInflater)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding?.btnGuide?.setOnClickListener {
+            findNavController().navigate(R.id.action_userGuideFragment_to_homeFruitsFragment)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_guide, container, false)
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UserGuideFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UserGuideFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
