bb3e33d871cadc0f1fc284ff9f6193ab95198e9c
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-12 07:21:41 +0700

    upgrade database version to 2

--- MD/app/src/main/java/com/darkshandev/freshcam/data/database/AppDatabase.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/database/AppDatabase.kt
@@ -8,7 +8,7 @@ import com.darkshandev.freshcam.data.models.HistoryClassificationEntity
 
 @Database(
     entities = [ClassifierLabelEntity::class, HistoryClassificationEntity::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
