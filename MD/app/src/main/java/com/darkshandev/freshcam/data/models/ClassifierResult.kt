b8a69a9e8550d2535348cf403664eb5c0cf0c86a
IzumiShaka-desu <shakaaji29@gmail.com>
2022-06-12 07:22:28 +0700

    implement parcelable to classifier result

--- MD/app/src/main/java/com/darkshandev/freshcam/data/models/ClassifierResult.kt
+++ MD/app/src/main/java/com/darkshandev/freshcam/data/models/ClassifierResult.kt
@@ -1,7 +1,11 @@
 package com.darkshandev.freshcam.data.models
 
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+@Parcelize
 data class ClassifierResult(
     val classifiedIndex: Int,
     val confidence: Float,
     val freshness: Boolean? = null
-)
+) : Parcelable
